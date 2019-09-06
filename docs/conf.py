f1e5450ca3a0ad667dc43353f7fe99c9cca1f2ad
justinlaughlin <justin@gb-uh.com>
2019-09-06 12:12:24 -0700

    conf

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+#master_doc = 'index'
+
+
 # -- Mock modules ------------------------------------------------------------
 import mock
 
