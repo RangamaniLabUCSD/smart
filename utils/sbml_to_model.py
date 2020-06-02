07433b3f5ce08cbb84b140e9c72c910728df5e01
willxu1234 <wlxu@ucsd.edu>
2020-06-02 13:58:53 -0700

    species support and unit fixes

--- utils/sbml_to_model.py
+++ utils/sbml_to_model.py
@@ -1,5 +1,5 @@
 from libsbml import *
-import sbml_unit_conversion
+from sbml_unit_conversion import *
 
 import sys
 import os
@@ -9,13 +9,13 @@ import stubs
 unit = stubs.unit
 
 # taken from https://www.ebi.ac.uk/biomodels/BIOMD0000000932#Files
-# fp = "models/Garde2020.xml"
+fp = "models/Garde2020.xml"
 
 # taken from https://www.ebi.ac.uk/biomodels/BIOMD0000000448#Files
 # fp = "models/BIOMD0000000448_url.xml"
 
 # taken from https://www.ebi.ac.uk/biomodels/BIOMD0000000931#Files
-fp = "models/Voliotis2019.xml"
+# fp = "models/Voliotis2019.xml"
 
 doc = readSBML(fp)
 if doc.getNumErrors() > 0:
@@ -23,21 +23,19 @@ if doc.getNumErrors() > 0:
 
 c_df = stubs.model_building.CompartmentDF()
 p_df = stubs.model_building.ParameterDF()
+r_df = stubs.model_building.ReactionDF()
+s_df = stubs.model_building.SpeciesDF()
 
-def sbml_to_compartment(document, compartment_df):
+def sbml_to_compartments(document, compartment_df):
     marker = 1
     model = document.getModel()
     compartments = model.getListOfCompartments()
     for compartment in compartments:
         s_id = compartment.getId()
-        units = compartment.getUnits()
-        if units == "":
-            units = "meter"
-        # units = compartment.getDerivedUnitDefinition()
-
+        u_def = compartment.getDerivedUnitDefinition()
+        units = sbml_unit_definition_to_pint_quantity(u_def)
         dim = compartment.getSpatialDimensions()
-        print(s_id, units, dim, marker)
-        compartment_df.append(s_id, dim, units, marker)
+        compartment_df.append(s_id, dim, units.units, marker)
         marker += 1
     return
 
@@ -47,14 +45,52 @@ def sbml_to_parameters(document, parameter_df):
     for parameter in parameters:
         s_id = parameter.getId()
         value = float(parameter.getValue())
-        units = parameter.getUnits()
+        u_def = parameter.getDerivedUnitDefinition()
+        units = sbml_unit_definition_to_pint_quantity(u_def)
+        # Conversion to SI units
+        value *= units.magnitude
         notes = parameter.getNotesString()
-        print(s_id, value, units)
-        parameter_df.append(s_id, value, units, notes)
+        parameter_df.append(s_id, value, units.units, notes)
     return
 
-sbml_to_compartment(doc, c_df)
+def sbml_to_reactions(document, reaction_df):
+    model = document.getModel()
+    reactions = model.getListOfReactions()
+    for reaction in reactions:
+        s_id = reaction.getId()
+        klaw = reaction.getKineticLaw()
+        klaw_math = klaw.getMath()
+        print(formulaToString(klaw_math))
+        # Relies on species; MathML doesn't type so you have to decide which
+        # leaf nodes of the AST are species, params, etc.
+    return
+
+def sbml_to_species(document, species_df):
+    model = document.getModel()
+    species_list = model.getListOfSpecies()
+    for species in species_list:
+        s_id = species.getId()
+        comp_name = species.getCompartment()
+        u_def = species.getDerivedUnitDefinition()
+        units = sbml_unit_definition_to_pint_quantity(u_def)
+        init_cond = species.getInitialConcentration()
+        # Convert to SI units
+        init_cond *= units.magnitude
+        group = species.getNotesString()
+        
+        # eventually determine diffusion and diffusion coefficients
+        species_df.append(s_id, group, units.units, init_cond, 0, '', comp_name)
+    pass
+
+
+
+sbml_to_compartments(doc, c_df)
 sbml_to_parameters(doc, p_df)
+# sbml_to_reactions(doc, r_df)
+sbml_to_species(doc, s_df)
 
 c_df.df.to_json(fp.split(".xml")[0] + "_compartments.json")
+p_df.df.to_json(fp.split(".xml")[0] + "_parameters.json")
+# r_df.df.to_json(fp.split(".xml")[0] + "_reactions.json")
+s_df.df.to_json(fp.split(".xml")[0] + "_species.json")
 
--- utils/sbml_unit_conversion.py
+++ utils/sbml_unit_conversion.py
@@ -19,14 +19,20 @@ SBML_BASE_UNIT_REGISTRY = {
     '[substance]': UNIT_KIND_MOLE
 }
 
-def convert_sbml_unit_to_pint(sbml_unit):
+def sbml_unit_to_pint_quantity(sbml_unit):
     kind = sbml_unit.kind
     scale = sbml_unit.scale
     exp = sbml_unit.exponent
     mult = sbml_unit.multiplier
     return (mult * SBML_BASE_UNIT_REGISTRY[kind]) ** exp * (10 ** scale)
 
-def convert_pint_to_sbml_unit_definition(pint_unit, model):
+def sbml_unit_definition_to_pint_quantity(sbml_unit_definition):
+    quantity = 1 * unit_registry.dimensionless
+    for unit in sbml_unit_definition.getListOfUnits():
+        quantity *= sbml_unit_to_pint_quantity(unit)
+    return quantity.to_compact()
+
+def pint_to_sbml_unit_definition(pint_unit, model):
     u_def = model.createUnitDefinition()
     u_def.setId(pint_unit.format_babel())
 
@@ -51,28 +57,26 @@ def extract_units_from_sbml_document(document):
         raise Exception("Errors detected while reading document.")
     return extract_units_from_sbml(document.getModel())
 
-def extract_units_from_sbml(model):
-    unit_defs = model.getListOfUnitDefinitions()
-    pint_units = {}
+# TODO: needs fixing to account for quantity return instead of unit return 
+# def extract_units_from_sbml(model):
+#     unit_defs = model.getListOfUnitDefinitions()
+#     pint_units = {}
 
-    for unit_def in unit_defs:
-        sid = unit_def.id
-        pint_unit = 1
-        units = unit_def.getListOfUnits()
-        for unit in units:
-            pint_unit *= convert_sbml_unit_to_pint(unit)
-        pint_units[sid] = pint_unit
+#     for unit_def in unit_defs:
+#         sid = unit_def.id
+#         pint_unit = sbml_unit_definition_to_pint_quantity(unit_def)
+#         pint_units[sid] = pint_unit
 
-    return pint_units
+#     return pint_units
 
 doc = SBMLDocument(3, 1)
 model = doc.createModel()
 u_a = 5 * unit_registry.kilometers ** 3
 u_b = 10 * unit_registry.molecules / unit_registry.miles ** 3
 u_c = 5 * unit_registry.inches / unit_registry.hours
-convert_pint_to_sbml_unit_definition(u_a.units, model)
-convert_pint_to_sbml_unit_definition(u_b.units, model)
-convert_pint_to_sbml_unit_definition(u_c.units, model)
+pint_to_sbml_unit_definition(u_a.units, model)
+pint_to_sbml_unit_definition(u_b.units, model)
+pint_to_sbml_unit_definition(u_c.units, model)
 print(writeSBMLToString(doc))
 
 # fp = "models/Voliotis2019.xml"
