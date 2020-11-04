67f151cc98bc3d367eedaa3017154e3c9f427548
Yuan Gao <y1gao@ucsd.edu>
2020-11-04 09:33:51 +0800

    Update toy_model_zerod.py

--- examples/toy_model/toy_model_zerod.py
+++ examples/toy_model/toy_model_zerod.py
@@ -11,7 +11,7 @@ r = stubs.model_building.ReactionDF()
 
 ### define parameters
 # name, value, unit, notes
-p.append('kf', 5.0, 1/(unit.uM*unit.s), 'forward rate')
+p.append('kf', 5.0, 1/((unit.molecule/unit.um**3)*unit.s), 'forward rate')
 p.append('kr', 0.1, 1/unit.s, 'reverse rate')
 p.append('kdeg', 2.0, 1/(unit.s), 'degradation rate')
 
@@ -19,13 +19,13 @@ p.append('kdeg', 2.0, 1/(unit.s), 'degradation rate')
 # name, plot group, concentration units, initial condition, diffusion
 # coefficient, diffusion coefficient units, compartment
 s.append('A', 'cytosolic', unit.molecule/unit.um**3, 10.0, 10, unit.um**2/unit.s, 'cyto')
-s.append('X', 'membrane bound', unit.molecule/unit.um**2, 1000.0, 0.1, unit.um**2/unit.s, 'pm')
-s.append('B', 'membrane bound', unit.molecule/unit.um**2, 0.0, 50, unit.um**2/unit.s, 'pm')
+s.append('X', 'cytosolic', unit.molecule/unit.um**3, 1000.0, 0.1, unit.um**2/unit.s, 'cyto')
+s.append('B', 'cytosolic', unit.molecule/unit.um**3, 0.0, 50, unit.um**2/unit.s, 'cyto')
 
 ### define compartments
 # name, geometric dimensionality, length scale units, marker value
 c.append('cyto', 3, unit.um, 1)
-c.append('pm', 2, unit.um, 2)
+#c.append('pm', 2, unit.um, 2)
 
 ### define reactions
 # name, notes, left hand side of reaction, right hand side of reaction, kinetic
