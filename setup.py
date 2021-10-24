f9b6653ae4f622bacee6fe018b37c061ee0328cf
jgl <justinglaughlin@gmail.com>
2021-10-24 11:43:36 -0700

    removed unnecessary git ignore

--- setup.py
+++ setup.py
@@ -3,19 +3,9 @@ stubs setup
 """
 import sys
 from setuptools import setup, find_packages
-import versioneer
 
-short_description = __doc__.split("\n")
-
-# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
-
-try:
-    with open("README.md", "r") as handle:
-        long_description = handle.read()
-except:
-    long_description = "\n".join(short_description[2:])
+with open("README.md", "r") as handle:
+    long_description = handle.read()
 
 
 setup(
@@ -23,25 +13,24 @@ setup(
     name='stubs',
     author='Justin Laughlin',
     author_email='justinglaughlin@gmail.com',
-    description=short_description[0],
+    url='https://github.com/justinlaughlin/stubs',
+    description='STUBS is a biophysical simulation library that provides a level of abstraction to models, making it easier for users to develop, share, and simulate their mathematical models.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    long_description_content_type='text/markdown',
+    platforms=['Linux', 'Mac OS-X'],
+    version='0.1.0',
     license='LGPLv3',
 
     # Which Python importable modules should be included when your package is installed
     # Handled automatically by setuptools. Use 'exclude' to prevent some specific
     # subpackage(s) from being added, if needed
-    packages=find_packages(),
+    #packages=find_packages(),
+    packages=['stubs'],
 
     # Optional include package data to ship with your package
     # Customize MANIFEST.in if the general case does not suit your needs
     # Comment out this line to prevent the files from being packaged with your software
-    include_package_data=True,
-
-    # Allows `setup.py test` to work correctly with pytest
-    setup_requires=[] + pytest_runner,
+    #include_package_data=True,
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
