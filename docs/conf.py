496e4f58235d757ebc8e2bfd869a98073f134517
justinlaughlin <justin@gb-uh.com>
2021-11-16 16:17:45 -0800

    requirements.txt for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Justin Laughlin'
 author = 'Justin Laughlin'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
