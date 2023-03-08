a572318608e5cb8440439c68b7a106d02f7cb747
Jørgen S. Dokken <dokken@simula.no>
2023-03-08 16:52:28 +0000

    Remove deprecations, improve docs.

--- stubs/__init__.py
+++ stubs/__init__.py
@@ -1,11 +1,10 @@
 from . import config
-from . import (common, data_manipulation, mesh, model, model_assembly,
+from . import (common, mesh, model, model_assembly,
                solvers, post_process)
 from .units import unit
 
 __all__ = [
     "config",
-    "data_manipulation",
     "model_assembly",
     "solvers",
     "model",
