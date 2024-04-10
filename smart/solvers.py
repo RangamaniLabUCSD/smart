57e5ffcee31955aaf43afb3d3f50bb464116e832
emmetfrancis <emmet.a.francis@gmail.com>
2024-04-09 23:41:52 -0700

    Detect when d.assemble_mixed returns nan and recompute

--- smart/solvers.py
+++ smart/solvers.py
@@ -9,6 +9,8 @@ import petsc4py.PETSc as p
 from .common import Stopwatch
 from .model_assembly import Compartment
 
+import numpy as np
+
 logger = logging.getLogger(__name__)
 
 __all__ = ["smartSNESProblem"]
@@ -350,7 +352,11 @@ class smartSNESProblem:
             Fvecs.append([])
             for k in range(len(self.Fforms[j])):
                 # , tensor=d.PETScVector(Fvecs[idx]))
-                Fvecs[j].append(d.as_backend_type(d.assemble_mixed(self.Fforms[j][k])))
+                # assemble_mixed sometimes returns nan (nondeterministic???)
+                cur_vec = d.assemble_mixed(self.Fforms[j][k])
+                while any(np.isnan(cur_vec.get_local())):
+                    cur_vec = d.assemble_mixed(self.Fforms[j][k])
+                Fvecs[j].append(d.as_backend_type(cur_vec))
             # TODO: could probably speed this up by not using axpy if there is only one subform
             # sum the vectors
             Fj_petsc[j].zeroEntries()
